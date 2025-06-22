126922dffe32682f4e5f91839cc2485dc3810272
Salem <salemmahmoud345@gmail.com>
2025-06-22 21:38:15 +0300

    The needed commit

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,8 @@
+number1 = 10
+number2 = 5
+Addition = number1 + number2
+Subtraction = number1 - number2
+Multiplication = number1 * number2
+print("Addition of 10 and 5 is", Addition)
+print("Subtraction of 10 and 5 is", Subtraction)
+print("Multiplication of 10 and 5 is", Multiplication)
