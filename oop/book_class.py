7e168b81bbb5328e55076c0c1c33f7ea23fc1b8c
Salem <salemmahmoud345@gmail.com>
2025-08-16 12:00:26 +0300

    The needed commit for book_class task

--- oop/book_class.py
+++ oop/book_class.py
@@ -3,10 +3,10 @@ class Book:
         self.title = title
         self.author = author
         self.year = year
-        
+
     def __str__ (self):
         return f"{self.title} by {self.author}, puplished in {self.year}"
-    def __repr__(self):
+    def __repr__ (self):
         return f"Book('{self.title}', '{self.author}', {self.year})"
     def __del__ (self):
         print(f"Deleting {self.title}")        
