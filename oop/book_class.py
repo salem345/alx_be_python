e4096d63ee6d55d8243649be713b24cb1c5f3a44
Salem <salemmahmoud345@gmail.com>
2025-08-16 13:08:18 +0300

    the final commit

--- oop/book_class.py
+++ oop/book_class.py
@@ -1,12 +1,12 @@
 class Book:
-    def __init__ (self,title,author,year):
+    def __init__(self,title,author,year):
         self.title = title
         self.author = author
         self.year = year
-
-    def __str__ (self):
-        return f"{self.title} by {self.author}, puplished in {self.year}"
-    def __repr__ (self):
-        return f"Book('{self.title}', '{self.author}', {self.year})"
-    def __del__ (self):
+        
+    def __del__(self):
         print(f"Deleting {self.title}")        
+    def __str__(self):
+        return f"{self.title} by {self.author}, puplished in {self.year}"
+    def __repr__(self):
+        return f"Book('{self.title}', '{self.author}', {self.year})"
