a9677f1c63d56016c441ca361d753710106c0d62
Salem <salemmahmoud345@gmail.com>
2025-08-17 13:06:06 +0300

    main

--- oop/main.py
+++ oop/main.py
@@ -1,14 +1,18 @@
-from polymorphism_demo import Shape, Rectangle, Circle
-import math
+from library_system import Book, EBook, PrintBook, Library
 
 def main():
-    shapes = [
-        Rectangle(10, 5),
-        Circle(7)
-    ]
+    my_library = Library()
+    classic_book = Book("Pride and Prejudice", "Jane Austen")
+    digital_novel = EBook("Snow Crash", "Neal Stephenson", 500)
+    paper_novel = PrintBook("The Catcher in the Rye", "J.D. Salinger", 234)
 
-    for shape in shapes:
-        print(f"The area of the {shape.__class__.__name__} is: {shape.area()}")
+    # Add books to the library
+    my_library.add_book(classic_book)
+    my_library.add_book(digital_novel)
+    my_library.add_book(paper_novel)
+
+
+    my_library.list_books()
 
 if __name__ == "__main__":
     main()
