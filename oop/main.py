42aeac21e9ee4b3c4fef4044e9ddd493f0c45786
Salem <salemmahmoud345@gmail.com>
2025-08-17 13:08:54 +0300

    main

--- oop/main.py
+++ oop/main.py
@@ -3,7 +3,7 @@ from library_system import Book, EBook, PrintBook, Library
 def main():
     my_library = Library()
     classic_book = Book("Pride and Prejudice", "Jane Austen")
-    digital_novel = EBook("Snow Crash", "Neal Stephenson", 500)
+    digital_novel = EBook("Snow Crash", "Neal Stephenson", "500KB")
     paper_novel = PrintBook("The Catcher in the Rye", "J.D. Salinger", 234)
 
     # Add books to the library
